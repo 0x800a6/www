032510d300e39f4ef8c96a30e9654d4c68164ea3
Lexi Rose Rogers <lexi@lrr.sh>
2025-10-01 05:43:17 -0400

    Remove Dotfiles and Friends links from navbar for a cleaner navigation experience

--- www/includes/navbar.php
+++ www/includes/navbar.php
@@ -28,13 +28,7 @@
           <a class="nav-link px-3" href="/posts">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link px-3" href="/dotfiles">Dotfiles</a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link px-3" href="/resume">Resume</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link px-3" href="/friends">Friends</a>
         </li>
         <li class="nav-item">
           <a class="nav-link px-3" href="/#about">About</a>
